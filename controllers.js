99be914691ecb6a0099a03ec8ecf8ca3c8dec7b0
Ben Stein <bumstain@gmail.com>
2016-05-31 13:54:41 +0100

    Added a controller to keep track of state

---
+++ controllers.js
@@ -0,0 +1,11 @@
+// angular.module('ToDoController', function(){
+//   var todo = this;
+//   todo = [
+//     {text: "ToDo1"}
+//   ]
+// });
+
+toDoApp.controller('ToDoController', function() {
+  this.todo = "ToDo1";
+});
+
